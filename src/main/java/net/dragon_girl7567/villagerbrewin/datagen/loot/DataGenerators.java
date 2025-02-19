fd9d93ceb215a858dcac3550473fc0f2d46cb70a
DragonGirl7567 <bethanymungar@gmail.com>
2025-02-18 21:03:45 -0500

    WIP Update

---
+++ src/main/java/net/dragon_girl7567/villagerbrewin/datagen/loot/DataGenerators.java
@@ -0,0 +1,27 @@
+package net.dragon_girl7567.villagerbrewin.datagen.loot;
+
+import net.dragon_girl7567.villagerbrewin.VillagerBrewin;
+import net.dragon_girl7567.villagerbrewin.datagen.ModPoiTypeTagsProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.concurrent.CompletableFuture;
+
+@Mod.EventBusSubscriber(modid = VillagerBrewin.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new ModPoiTypeTagsProvider(packOutput, lookupProvider, existingFileHelper));
+
+    }
+}
